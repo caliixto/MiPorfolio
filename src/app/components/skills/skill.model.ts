809e2f265ea20ce58eb6b6ba838b45b9ed1e56b6
DESKTOP-B3V34VL\calixto <nicolas3jdd@gmail.com>
2026-06-15 13:25:02 +0200

    seccion skills

---
+++ src/app/components/skills/skill.model.ts
@@ -0,0 +1,5 @@
+export interface Skill {
+  name: string;
+  image: string;
+  category: 'lenguaje' | 'framework' | 'herramienta' | 'base-datos' | 'libreria';
+}
--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import { Skill } from './skill.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
-export class Skills {}
+export class Skills {
+  currentFilter:string = "todas";
+
+  skills: Skill[] = [
+    // Lenguajes
+    { name: 'HTML', image: '/img/html.png', category: 'lenguaje' },
+    { name: 'CSS', image: '/img/css.webp', category: 'lenguaje' },
+    { name: 'JavaScript', image: '/img/js.png', category: 'lenguaje' },
+    { name: 'TypeScript', image: '/img/typescript.png', category: 'lenguaje' },
+    { name: 'SQL', image: '/img/sql.png', category: 'lenguaje' },
+    
+    // Frameworks y Entornos (Backend)
+    { name: 'Angular', image: '/img/angular.png', category: 'framework' },
+    { name: 'Node.js', image: '/img/node.png', category: 'framework' },
+    
+    // Bases de Datos
+    { name: 'MongoDB', image: '/img/mongo.jpg', category: 'base-datos' },
+    
+    // Herramientas y Librerías
+    { name: 'Git', image: '/img/git.png', category: 'herramienta' },
+    { name: 'SweetAlert2', image: '/img/sweetalert2.png', category: 'libreria' }
+  ];
+
+  displaySkills = this.skills;
+
+  filter (category:string){
+    this.currentFilter = category;
+    if (category == "todas"){
+      this.displaySkills = this.skills;
+    }else{
+      this.displaySkills = this.skills.filter(s=> s.category==category);
+    }
+  }
+}
